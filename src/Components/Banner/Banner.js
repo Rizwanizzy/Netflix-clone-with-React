80719307336db22786d1d41a2860c78ee8df8576
Mohammed Rizwan <mohammedrizwanizzy@gmail.com>
2023-08-01 13:06:02 +0530

    front end design almost completed

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie name</h1>
+            <div className='banner_buttons'>
+                <button className='button'>Play</button>
+                <button className='button'>My list</button>
+            </div>
+            <h1 className='description'>
+                Lorem Ipsum is simply dummy text of 
+                the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's 
+                
+            </h1>
+        </div>
+        <div className="fade_bottom">
+
+        </div>
+    </div>
+  )
+}
+
+export default Banner
